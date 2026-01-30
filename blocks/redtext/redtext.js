b1728a0b978e2dcdbf005a2910ce479cd8445774
Lakshmig-23 <lakshmi.c.g@capgemini.com>
2026-01-30 08:33:52 +0530

    Create redtext.js

---
+++ blocks/redtext/redtext.js
@@ -0,0 +1,11 @@
+
+export default function decorate(block) {
+  // Add a small effect on hover using JavaScript (optional)
+  block.addEventListener('mouseenter', () => {
+    console.log('Hover started on red text block');
+  });
+
+  block.addEventListener('mouseleave', () => {
+    console.log('Hover ended on red text block');
+  });
+}
